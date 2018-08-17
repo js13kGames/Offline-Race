d3795afac31252b94ca641fccb6c737f27dc0706
Leandro <ext3-cti@ctisoluciones.com>
2018-08-17 13:42:38 +0200

    improved render tiles performance

--- public/ui.js
+++ public/ui.js
@@ -9,17 +9,18 @@ const UI = {
   setMessage: (m) => msg.innerHTML =  m,
   drawBoard: (tiles) => {
     const HALF_TILE = TILE_SIZE / 2;
+    let strTiles = '';
     tiles.forEach(function(t) {
       const pos = t.split;
-      addToBoard(
+      strTiles +=
         `<g>
           <rect x=${t.i * TILE_SIZE} y=${t.j * TILE_SIZE} width=${TILE_SIZE} height=${TILE_SIZE} fill='gray' stroke='white' stroke-width='2'/>
           <text x=${t.i * TILE_SIZE + HALF_TILE} y=${t.j * TILE_SIZE + HALF_TILE} fill="white" font-size="4vh" text-anchor="middle" alignment-baseline="central">
             ${t.v}
           </text>
         </g>`
-      );
     });
+    addToBoard(strTiles);
   },
   clearBoard: () => board.innerHTML = ''
 }
